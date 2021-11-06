79e2fec98c0764a9473816a2604a96f467c53887
Muhammed Ali <muhamzyali@gmail.com>
2021-11-06 21:41:07 +0100

    fail test

--- blog/tests/test_models.py
+++ blog/tests/test_models.py
@@ -4,12 +4,12 @@ from blog.models import Article
 
 @pytest.mark.django_db
 
-def test_article_create():
-    article = Article.objects.create(
-    author_name="Muhammed Ali",
-    title="Simple article",
-    content="The article's content",
-    )
-    assert article.author_name=="Muhammed Ali"
-    assert article.title=="Simple article"
-    assert article.content=="The article's content"
+def test_article_create():  
+   article = Article.objects.create(  
+   author_name="Muhammed Ali",  
+   title="Simple article",  
+   content="The article's content",  
+   )  
+   assert article.author_name=="Muhammed Al" # this will make sure the test fails  
+   assert article.title=="Simple article"  
+   assert article.content=="The article's content" 
